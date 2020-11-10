c42cadc15ee0d2545e26719a203c6bf7d9ffb77c
amiS-acat <sam11204st@gmial.com>
2020-11-10 22:46:00 +0900

    sassDown

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,14 +47,14 @@ module.exports = {
     //     plugins: [],
     //   },
     // },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: process.env.CONTENTFUL_HOST,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-contentful`,
+    //   options: {
+    //     spaceId: process.env.CONTENTFUL_SPACE_ID,
+    //     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    //     host: process.env.CONTENTFUL_HOST,
+    //   },
+    // },
   ],
 }
 
