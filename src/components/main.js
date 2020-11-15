63243f364094f6fa846f46f1b715f3fdc8db41eb
amiS-acat <sam11204st@gmial.com>
2020-11-15 10:40:12 +0900

    コミット！

--- src/components/main.js
+++ src/components/main.js
@@ -1,16 +1,35 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import { Carousel } from "react-bootstrap"
+import Img from "gatsby-image"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Main = () => {
-    // const data = useStaticQuery(graphql`
-    // query
-    // `)
+    const data = useStaticQuery(graphql`
+    query{
+        file(relativePath: {eq: "paper.jpg"}) {
+          childImageSharp {
+            fluid(maxWidth: 550) {
+                src
+                srcSet
+                srcSetWebp
+                srcWebp
+                base64
+                aspectRatio
+                sizes
+            }
+          }
+        }
+    }
+    `)
 
     return (
         <div className="maincontainer">
             <div className="maincontainer__inner">
                 <div className="main">
                     <div className="main__inner">
+                        <h2>Playlist 1</h2>
                         <div className="video">
                             <div className="iframe__outer">
                                 <iframe
@@ -23,13 +42,32 @@ const Main = () => {
                         </div>
                         <div className="iframe__under">
                         </div>
-                        <div className="video__descript">
-                            test
+                        <div className="index">
+                            <div className="index__alph">Doctor Gradus ad parnassum/debussy</div>
+                            <p class="index__ja">グラドゥス・アド・パルナッスム博士/ドビュッシー</p>
+                            <div className="index__alph">La marchande d'eau fraîche/ibert</div>
+                            <p class="index__ja">水売り女/イベール</p>
+                            <div className="index__alph">Sicilienne/Koechlin</div>
+                            <p class="index__ja">シシリエンヌ/ケクラン</p>
+                            <div className="index__alph">The Fountain of the Aqua Paola/Griffis</div>
+                            <p class="index__ja">アクア・パオラ/グリフィス</p>
+                            <div className="index__alph">Sonatine No1."Modere"/Ravel</div>
+                            <p class="index__ja">ソナチネ No1.モデラート/ラヴェル</p>
+                            <div className="index__alph">Rhapsody from Theam of paganini　Variation18/Rachmaninov</div>
+                            <p class="index__ja">パガニーニの主題による狂詩曲 第18変奏/ラフマニノフ</p>
+                            <div className="index__alph">Je te veux/Satie</div>
+                            <p class="index__ja">ジュ トゥ ヴー/サティ</p>
+                            <div className="index__alph">Equinox for Piano/Ireland</div>
+                            <p class="index__ja">イークィノクス/アイアランド</p>
+                            <div className="index__alph">Liszt/Love Dream　no.3</div>
+                            <p class="index__ja">愛の夢　第3番/リスト</p>
+                            <div className="index__alph">Minute Waltz/Chopin</div>
+                            <p class="index__ja">子犬のワルツ/ショパン</p>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
+                </div >
+            </div >
+        </div >
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react"
 
 import Layout from "../components/layout"
 import Main from "../components/main"
+import FirstView from "../components/firstView"
 
 const Index = () => {
   return (
     <>
       <Layout>
+        <FirstView />
         <Main />
       </Layout>
     </>
