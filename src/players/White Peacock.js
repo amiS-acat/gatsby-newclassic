7e9c92fceb0217f731eb52c014e38fc46ed4379c
amiS-acat <sam11204@gmail.com>
2021-02-06 18:32:17 +0900

    try play

--- src/players/White Peacock.js
+++ src/players/White Peacock.js
@@ -3,36 +3,37 @@ import useSound from "use-sound";
 
 import music from "../../frontmatter/music/White Peacock.mp3";
 
-const Pause = ({ stop }) => {
-  return (
-    <div
-      className="player"
-      onClick={() => stop()}
-      onKeyDown={() => stop()}
-    ></div>
-  );
-};
+// const Pause = ({ stop }) => {
+//   return (
+//     <div
+//       className="player"
+//       onClick={() => stop()}
+//       onKeyDown={() => stop()}
+//     ></div>
+//   );
+// };
 
-const Play = ({ play, stop }) => {
-  return (
-    <div
-      className="player"
-      onClick={(() => stop(), play)}
-      onKeyDown={(() => stop(), play)}
-    ></div>
-  );
-};
+// const Play = ({ play, stop }) => {
+//   return (
+//     <div
+//       className="player"
+//       onClick={(() => stop(), play)}
+//       onKeyDown={(() => stop(), play)}
+//     ></div>
+//   );
+// };
 
 const Player = () => {
-  const [play, { stop, isPlaying }] = useSound(music);
+//   const [play, { stop, isPlaying }] = useSound(music);
 
-  useEffect(() => {
-    return () => {
-      stop();
-    };
-  }, [stop]);
+//   useEffect(() => {
+//     return () => {
+//       stop();
+//     };
+//   }, [stop]);
 
-  return <div>{isPlaying ? <Pause stop={stop} /> : <Play play={play} />}</div>;
+  return <audio controls src={music} />
+          // <div>{isPlaying ? <Pause stop={stop} /> : <Play play={play} />}</div>;
 };
 
 export default Player;
